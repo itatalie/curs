df395d799760a4245abe3b55afc266d1b9c28e73
tatalie <tattaasmolentseva@gmail.com>
2025-06-08 01:15:50 +0300

    curses

--- src/reports.py
+++ src/reports.py
@@ -33,3 +33,4 @@ def spending_by_category(transactions, category, date=None):
     except Exception as e:
         logging.error(f"Ошибка при формировании отчета: {e}")
         return json.dumps({"error": "Произошла ошибка при формировании отчета"}, ensure_ascii=False, indent=4)
+
